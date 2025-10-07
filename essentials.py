16d9085543e465f7c9c24d3cdac24e03b980101e
corrupted-application <96342169+corrupted-application@users.noreply.github.com>
2025-10-07 17:01:03 +0200

    fix for POSIX systems (0.0.3.2)

--- essentials.py
+++ essentials.py
@@ -1,11 +1,12 @@
 """
 Essentials by corrupted-application
-Version 0.0.3.1
+Version 0.0.3.2
 """
 
 import subprocess
 import os
-import winsound
+if os.name == 'nt':
+  import winsound # fix for POSIX systems, would result in hang due to winsound being loaded, which is not available on POSIX systems
 
 def clear():
  if os.name == "posix":
@@ -17,31 +18,31 @@ def clear():
 
 def title(title_name):
  if os.name == "posix":
-   print(f"\033]0;{title_name}\007", end="")
+  print(f"\033]0;{title_name}\007", end="")
  elif os.name == "nt":
-   os.system(f'title {title_name}')
+  os.system(f'title {title_name}')
  else:
-    print("[essentials]: Something went wrong during title command execution, or your operating system does not support title changes")
+   print("[essentials]: Something went wrong during title command execution, or your operating system does not support title changes")
 
 def version():
-    print("[essentials]: Essentials 0.0.3.1")
+  print("[essentials]: Essentials 0.0.3.2")
 
 def beep(frequency=800, duration=500):
  if os.name == "nt":
-     beep_nt(frequency, duration)
+    beep_nt(frequency, duration)
  elif os.name == "posix":
-     beep_pos() # calls to beep_posix, still works only if terminal supports bell
+    beep_pos() # calls to beep_posix, still works only if terminal supports bell
  else:
-     print("[essentials]: Something went wrong during beep command execution, or your operating system does not support beep command execution.")
+    print("[essentials]: Something went wrong during beep command execution, or your operating system does not support beep command execution.")
 
 def beep_nt(frequency=800, duration=500):
  if os.name == "nt":
-    winsound.Beep(frequency, duration) # opted for winsound.Beep instead of powershell for better support for older windows hosts without powershell
+   winsound.Beep(frequency, duration) # opted for winsound.Beep instead of powershell for better support for older windows hosts without powershell
  else:
-    print("[essentials]: Something went wrong during beep_nt command execution, or you are running it on an operating system different from Windows, which beep_nt does not support.")
+   print("[essentials]: Something went wrong during beep_nt command execution, or you are running it on an operating system different from Windows, which beep_nt does not support.")
 
 def beep_pos():
  if os.name == "posix":
-     os.system("echo -e '\a'")  # bell, only works if terminal supports it
+    os.system("echo -e '\a'")  # bell, only works if terminal supports it
  else:
-     print("[essentials]: Something went wrong during beep_pos command execution, or you are running it on a non-unix based operating system, which beep_pos does not support.")
+    print("[essentials]: Something went wrong during beep_pos command execution, or you are running it on a non-unix based operating system, which beep_pos does not support.")
